ad1b4483fec58f3149a8beb85df429bc92ad59f7
Chanhyuk Byeon <bdu00chch@gmail.com>
2022-11-12 19:26:57 +0900

    editorconfig, prettier, eslint 세팅

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,14 @@
-import express from "express";
-import dotenv from "dotenv";
+import express from 'express';
+import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT ?? "8080";
+const port = process.env.PORT ?? '8080';
 
-app.get("/", (req, res) => {
-  res.send("Express + TypeScript Server");
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+
+  console.log('123');
 });
 
 app.listen(port, () => {
